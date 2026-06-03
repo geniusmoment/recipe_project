89d2808c4f17c1d8d63522c03fff1118a6020ddf
geniusmoment <arbibishev@edu.hse.ru>
2026-06-03 22:51:01 +0300

    Написание тестов для Recipe

--- test_recipes.py
+++ test_recipes.py
@@ -33,3 +33,62 @@ class TestIngredient:
     def test_eq(self, ingredient_a, ingredient_b, expected):
         actual = (ingredient_b == ingredient_a)
         assert actual == expected
+
+class TestRecipe:
+
+    def test_recipe_creation(self):
+        ingredient_a = Ingredient("Бананы", 3, "кг")
+        ingredient_b = Ingredient("Молоко", 1, "кг")
+        recipe = Recipe("Коктейль", [ingredient_a, ingredient_b])
+        assert recipe.title == 'Коктейль'
+        assert len(recipe) == 2
+        assert recipe.ingredients[0].name == 'Бананы'
+
+    def test_add_ingredient_new(self):
+        recipe = Recipe("Коктейль")
+        new_ingredient = Ingredient("Клубника", 100, "г")
+        recipe.add_ingredient(new_ingredient)
+        actual = len(recipe.ingredients)
+        expected = 1
+        assert actual == expected
+        assert recipe.ingredients[0].name == "Клубника"
+        assert recipe.ingredients[0].quantity == 100.0
+
+    def test_add_ingredient_duplicate(self):
+        ingredient_a = Ingredient("Мясо", 2, "кг")
+        ingredient_b = Ingredient("Мясо", 3, "кг")
+        recipe = Recipe("Шашлык", [ingredient_a])
+        recipe.add_ingredient(ingredient_b)
+        actual = len(recipe.ingredients)
+        expected = 1
+        assert actual == expected
+        actual_2 = recipe.ingredients[0].quantity
+        expected_2 = 5.0
+        assert actual_2 == expected_2
+
+    def test_scale(self):
+        ingredient_a = Ingredient("Пиво", 999, "мл")
+        ingredient_b = Ingredient("Водка", 500, "мл")
+        original_recipe = Recipe("Хороший вечер", [ingredient_a, ingredient_b])
+        ratio = 2.0
+        scaled_recipe = original_recipe.scale(ratio)
+        assert scaled_recipe is not original_recipe
+        assert scaled_recipe.ingredients[0].quantity == 1998.0 
+        assert scaled_recipe.ingredients[1].quantity == 1000.0
+        assert scaled_recipe.title == original_recipe.title
+
+    def test_invalid_scale(self):
+        ingredient = Ingredient("Порошок", 200, "г")
+        recipe = Recipe("Какао", [ingredient])
+        invalid_ratio = -2.0
+        with pytest.raises(ValueError):
+            recipe.scale(invalid_ratio)
+    
+    def test_len(self):
+        ingredient_a = Ingredient("Горох", 200, "г")
+        ingredient_b = Ingredient("Бобы", 150, "г")
+        ingredient_c = Ingredient("Горох", 50, "г")
+        recipe = Recipe("Бобовый горох", [ingredient_a, ingredient_b, ingredient_c])
+        actual = len(recipe)
+        expected = 2
+        assert actual == expected
